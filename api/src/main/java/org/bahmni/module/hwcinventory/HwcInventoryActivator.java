b0f7594d8ce98d057282b6cfe6d192fb14c818e6
deepthi-mantena <96411257+deepthi-mantena@users.noreply.github.com>
2023-06-28 18:20:20 +0530

    BAH-101 [Deepthi Mantena/Pooja Deshpande]updated the configurations  (#2)
    * BAH-101 | updated the group id ,added .gitignore file and made minor changes to remove unused code
    
    * BAH-101 | Updated to resolve issue with uploading omod in openmrs modules.
    
    * BAH-101[Deepthi Mantena/Pooja Deshpande] | Refactored logging message of activator class .

---
+++ api/src/main/java/org/bahmni/module/hwcinventory/HwcInventoryActivator.java
@@ -0,0 +1,19 @@
+package org.bahmni.module.hwcinventory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.BaseModuleActivator;
+
+public class HwcInventoryActivator extends BaseModuleActivator {
+    private final Log log = LogFactory.getLog(this.getClass());
+
+    @Override
+    public void started() {
+        log.info("Started Bahmni hwcinventory module");
+    }
+
+    @Override
+    public void stopped() {
+        log.info("Stopped Bahmni hwcinventory module");
+    }
+}
